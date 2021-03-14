d4172d3109ec10a72e7fc18c28247b4401659ff1
HarryPan <harrypan311@gmail.com>
2021-03-13 20:14:38 -0500

    Revert "Merge branch 'main' into RenderTest"
    This reverts commit e998b985a23fa4383410606d20206f171b07df8d, reversing
    changes made to 36a44d39be8dafa6b29f5e28db49e158fbeed198.

--- ThermalRender/ThermalRender/main.cpp
+++ ThermalRender/ThermalRender/main.cpp
@@ -1,43 +1,108 @@
-
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include "cuda_gl_interop.h"
 #include "Render.cuh"
-#include <string>
-#include <fstream>
 
+GLFWwindow* window;
 const int WINDOW_WIDTH = 1280, WINDOW_HEIGHT = 720;
-const int TargetSample = 512;
+GLuint pbo, textureID;
+cudaGraphicsResource_t pboCuda;
+int Samples = 0;
+
+void printGlInfo()
+{
+	std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+	std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+}
+
+void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+void initOpenGL() {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	if (!glfwInit()) {
+		std::cout << "GLFW Init Failed" << std::endl;
+	}
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Thermal Render", NULL, NULL);
+	if (!window) {
+		std::cout << "Window Creation Failed" << std::endl;
+	}
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW initialization failed.\n";
+	}
+	glfwSwapInterval(1);
+	glfwSetErrorCallback(error_callback);
+
+	glGenBuffers(1, &pbo);
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+	glBufferData(GL_PIXEL_UNPACK_BUFFER, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(float) * 4, 0, GL_DYNAMIC_COPY);
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, WINDOW_WIDTH, WINDOW_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	gpuErrchk(cudaGraphicsGLRegisterBuffer(&pboCuda, pbo, cudaGraphicsRegisterFlagsNone));
+
+	glEnable(GL_FRAMEBUFFER_SRGB);//Gamma Correction
+}
+
+void DrawTexture() {
+	glUseProgram(0);
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glEnable(GL_TEXTURE_2D);
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT,
+		GL_RGBA, GL_FLOAT, NULL);
+
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 0.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 0.0f);
+	glEnd();
+
+	glDisable(GL_TEXTURE_2D);
+}
+
+void DrawPBO() {
+	gpuErrchk(cudaGraphicsMapResources(1, &pboCuda));
+
+	float* d_pbo;
+	size_t numBytes = WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(float) * 4;
+	gpuErrchk(cudaGraphicsResourceGetMappedPointer((void**)&d_pbo, &numBytes, pboCuda));
+
+	render(d_pbo, WINDOW_WIDTH, WINDOW_HEIGHT, Samples);
+
+	gpuErrchk(cudaGraphicsUnmapResources(1, &pboCuda));
+}
 
 int main() {
-	
+	initOpenGL();
 	initRender(WINDOW_WIDTH, WINDOW_HEIGHT);
-	int Samples = 0;
-	float* d_data, *h_data;
-	size_t data_size = 11 * WINDOW_WIDTH * WINDOW_HEIGHT;
-	gpuErrchk(cudaMalloc((void**)&d_data, data_size * sizeof(float)));
-	while (Samples < TargetSample) {
-		render(d_data, WINDOW_WIDTH, WINDOW_HEIGHT, Samples);
-		printf("Sample: %i\n", Samples);
-	}
 
-	h_data = new float[11 * WINDOW_WIDTH * WINDOW_HEIGHT];
-	gpuErrchk(cudaMemcpy(&h_data[0], &d_data[0], data_size * sizeof(float), cudaMemcpyDeviceToHost));
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+		//Draw
+		DrawPBO();
+		DrawTexture();
 
-	std::string name = "output/Human&Car_" + std::to_string(TargetSample) + ".txt";
-	std::ofstream outFile(name);
-	for (int y = 0; y < WINDOW_HEIGHT; y++) {
-		for (int x = 0; x < WINDOW_WIDTH; x++) {
-			float* ptr = h_data + 11 * (y * WINDOW_WIDTH + x);
-			for (int i = 0; i < 11; i++) {
-				outFile << *(ptr + i);
-				outFile << " ";
-			}
-			outFile << "\t";
-		
-		}
-		outFile << "\n";
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
-	outFile.close();
-	gpuErrchk(cudaFree(d_data));
-	delete h_data;
 	return 0;
 }
